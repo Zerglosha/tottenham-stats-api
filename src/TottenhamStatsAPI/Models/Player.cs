8f9e843e675417961fb2419f40091f9c662a3c19
Danila Aleksandrov <danila.alexandroff@gmail.com>
2026-06-10 22:04:07 +0300

    Add player entity and make migration for applying it to the DB

--- src/TottenhamStatsAPI/Models/Player.cs
+++ src/TottenhamStatsAPI/Models/Player.cs
@@ -2,5 +2,12 @@ namespace TottenhamStatsAPI.Models;
 
 public class Player
 {
-    
+    public int PlayerId { get; set; }
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public int SquadNumber { get; set; }
+    public int Apperances { get; set; }
+    public int Goals { get; set; }
+    public int Assists { get; set; }
+    public bool IsInjured { get; set; }
 }
